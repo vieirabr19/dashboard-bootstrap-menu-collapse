bf645c51e6f94e19bfdeb6f50281693cbc4d2bac
Luiz Vieira <luiz22_sp@hotmail.com>
2023-10-23 22:03:56 -0300

    Projeto dashboard bootstap menu collapse - final

--- js/script.js
+++ js/script.js
@@ -2,3 +2,30 @@ const sidebarToggle = document.querySelector("#sidebar_toggle");
 sidebarToggle.addEventListener("click", () => {
   document.querySelector("#sidebar").classList.toggle("collapsed");
 });
+
+document.querySelector(".theme-toggle").addEventListener("click", () => {
+  toogleLocalStorage();
+  toogleRootClass();
+});
+
+function toogleRootClass() {
+  const current = document.documentElement.getAttribute("data-bs-theme");
+  const inverted = current === "dark" ? "light" : "dark";
+  document.documentElement.setAttribute("data-bs-theme", inverted);
+}
+
+function toogleLocalStorage() {
+  if (isLightTheme()) {
+    localStorage.removeItem("light");
+  } else {
+    localStorage.setItem("light", "set");
+  }
+}
+
+function isLightTheme() {
+  return localStorage.getItem("light");
+}
+
+if (isLightTheme()) {
+  toogleRootClass();
+}
